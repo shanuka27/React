1fa8e177f7e68f364ae4b2fd4552afe5bc2f4345
shanuka27 <shanukayasiru27@gmail.com>
2023-01-09 23:48:47 +0530

    Navbar

---
+++ React-site/react_website/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path='/' exact />
+          </Routes>
+      </BrowserRouter>
+      
+    </>
+        
+    
+  );
+}
+
+export default App;
